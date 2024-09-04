bdb92a152641d1df808c8e9e222231f4e7250341
Daniel Edrisian <dedrisian@gmail.com>
2024-09-04 09:10:23 -0700

    Save file at app/(tabs)/explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -7,9 +7,6 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-
-
-
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
